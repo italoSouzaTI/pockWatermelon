c0e548852d3ec0590409a7c828ac9e9c1c6b846d
italo Souza <italoasouzat.i@gmail.com>
2025-06-27 10:39:21 -0300

    feat: tabela de log

--- src/features/pages/Home/useHomeModelView.ts
+++ src/features/pages/Home/useHomeModelView.ts
@@ -1,28 +1,34 @@
 import { database } from "@core/dataBase";
 import { nameTable } from "@core/dataBase/nameTable";
 import { QueryDB } from "@core/dataBase/QueryDB";
-import { useIsFocused } from "@react-navigation/native";
+import { useIsFocused, useNavigation, useRoute } from "@react-navigation/native";
 import { useEffect, useRef, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
-import { Q } from "@nozbe/watermelondb";
 import { HomeRepository } from "@features/api/repository/home.repository";
-import { syncDB } from "@features/api/repository/sync.repository";
+import { Logrepository } from "@features/api/repository/log.repository";
 
 export function useHomeModelView() {
     const { top } = useSafeAreaInsets();
+    const params = useRoute();
     const isFocused = useIsFocused();
-    const [startSend, setStartSend] = useState(false);
+    const navigation = useNavigation();
     const [data, setData] = useState([]);
     const { getDB } = QueryDB();
     const { processandoDados } = HomeRepository();
+    const { criandoLog } = Logrepository();
     const locationRef = useRef<Location.LocationObject>(null);
 
     async function transformandoDados() {
         try {
             let clientes = await getDB(nameTable.cliente);
+            let log = await getDB(nameTable.log);
+            let teste = log.map((ctx) => {
+                return ctx._raw;
+            });
             console.log("clientes", clientes);
+            console.log("log", teste);
             const localizacaes = await getDB(nameTable.localizacao);
             if (localizacaes.length == 0) {
                 setData(clientes);
@@ -54,6 +60,18 @@ export function useHomeModelView() {
                 ]);
                 return;
             }
+            let params = {
+                action: "atualizarDados",
+                details: {
+                    details: "Processando Dados",
+                    components: "renderItem",
+                    processandoDados: id,
+                    locationRef,
+                    created_at: new Date().getTime(),
+                },
+                screen: "Home",
+            };
+            await criandoLog(params);
             await processandoDados(id, locationRef);
             await transformandoDados();
         } catch (error) {
@@ -70,17 +88,46 @@ export function useHomeModelView() {
         let location = await Location.getCurrentPositionAsync({});
         locationRef.current = location;
     }
+    async function handleForm() {
+        try {
+            let params = {
+                action: "handleForm",
+                details: {
+                    handleForm: "navegando para tela de formulário de cadastro.",
+                    created_at: new Date().getTime(),
+                },
+                screen: "Home",
+            };
+            await criandoLog(params);
+            navigation.navigate("Form");
+        } catch (error) {}
+    }
+    async function handleHistorico(item: any) {
+        try {
+            let params = {
+                action: "handleHistorico",
+                details: {
+                    details: "Visualizando dados usuário",
+                    handleHistorico: { id: item.id, nome: item.nome },
+                    created_at: new Date().getTime(),
+                },
+                screen: "Home",
+            };
+            await criandoLog(params);
+            navigation.navigate("History", { id: item.id, nome: item.nome });
+        } catch (error) {}
+    }
     useEffect(() => {
         getCurrentLocation();
     }, []);
     useEffect(() => {
         transformandoDados();
-        syncDB();
     }, [isFocused]);
     return {
         top,
-        startSend,
         data,
+        handleForm,
         atualizarDados,
+        handleHistorico,
     };
 }
